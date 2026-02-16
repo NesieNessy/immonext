de20a6b64bbda0461120e7d29bb8f9f01d6065d9
Vanessa Zwing <vanessa.zwing@bmw.de>
2026-02-16 17:49:53 +0100

    Remove Typescript Errors Ignore

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  typescript: {
-    // Temporarily ignore TypeScript errors during build
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
