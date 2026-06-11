da6583c8ae0f41242bb6c1c289630495cb4ea921
Vanessa Zwing <vanessa.zwing@bmw.de>
2026-06-11 11:03:20 +0200

    fix: local build

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: (process.env.BUILD_OUTPUT as 'standalone' | 'export' | undefined) ?? 'standalone',
+  output: (process.env.BUILD_OUTPUT as 'standalone' | 'export' | undefined) ?? undefined,
   images: {
     unoptimized: true,
   },
